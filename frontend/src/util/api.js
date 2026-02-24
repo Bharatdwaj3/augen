c71f6600d9288bef64743d640524f7085d6c94c9
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2026-02-24 21:18:12 +0530

    edited api.js file

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
-
+const API_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5001' 
+  : import.meta.env.VITE_API_URL;
 const api = axios.create({
   baseURL: `${API_URL}/api`,
   withCredentials: true,
